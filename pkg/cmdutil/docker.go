b6d17fd58e6a8cf4b6e202724f6f5a75f2e93c44
陌竹 <mozhu233@outlook.com>
2023-09-21 02:14:12 +0800

    ✨ Feat(docker): cmd runtime output

--- pkg/cmdutil/docker.go
+++ pkg/cmdutil/docker.go
@@ -1,22 +1,74 @@
 package cmdutil
 
 import (
-	"bytes"
+	"bufio"
 	"fmt"
+	"io"
+	"log"
+	"os"
 	"os/exec"
 )
 
-func exec_cmd(command string) (string, string, error) {
-	var stdout bytes.Buffer
-	var stderr bytes.Buffer
+func exec_cmd(command string) {
 	cmd := exec.Command("/bin/bash", "-c", command)
 	cmd.Env = append(cmd.Env, "COMPOSE_DOCKER_CLI_BUILD=1", "DOCKER_BUILDKIT=1")
-	cmd.Stdout = &stdout
-	cmd.Stderr = &stderr
-	err := cmd.Run()
-	fmt.Print(stderr.String())
-	fmt.Print(stdout.String())
-	return stdout.String(), stderr.String(), err
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		log.Fatalln("stderr pipe ", err)
+	}
+	defer stderr.Close()
+	if err := cmd.Start(); err != nil {
+		log.Fatalln("start ", err)
+	}
+	go func() {
+		serr := bufio.NewReader(stderr)
+		for {
+			line, _, err2 := serr.ReadLine()
+			if err2 != nil || io.EOF == err2 {
+				break
+			}
+			fmt.Println(string(line))
+		}
+	}()
+	if err := cmd.Wait(); err != nil {
+		log.Fatalln("wait ", err)
+	}
+}
+
+// Deprecated 有 Bug 不建议使用
+func exec_cmd_attach(command string) {
+	cmd := exec.Command("/bin/bash", "-c", command)
+	log.Println(cmd.Args)
+	cmd.Env = append(cmd.Env, "COMPOSE_DOCKER_CLI_BUILD=1", "DOCKER_BUILDKIT=1", "COMPOSE_INTERACTIVE_NO_CLI=1")
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		log.Fatalln("stdin pipe ", err)
+	}
+	defer stdin.Close()
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatalln("stdout pipe ", err)
+	}
+	defer stdout.Close()
+	if err := cmd.Start(); err != nil {
+		log.Fatalln("start ", err)
+	}
+	go func() {
+		out := bufio.NewReader(stdout)
+		for {
+			line, _, err2 := out.ReadLine()
+			if err2 != nil || io.EOF == err2 {
+				break
+			}
+			fmt.Println(string(line))
+		}
+	}()
+
+	io.Copy(stdin, bufio.NewReader(os.Stdin))
+
+	if err := cmd.Wait(); err != nil {
+		log.Fatalln("wait ", err)
+	}
 }
 
 func Auto() {
@@ -39,7 +91,7 @@ func Stop() {
 }
 
 func Bash() {
-	exec_cmd("docker-compose exec challenge bash")
+	exec_cmd_attach("docker-compose exec challenge sh")
 }
 
 func Log() {
